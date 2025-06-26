1518f12722223da7b77699a1e6e4cb6825a9f7d1
MoRsHeD <mdmorshed0187@gmail.com>
2025-06-26 12:08:54 +0600

    created project

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+
+const Notfound = () => {
+    return (
+        <>
+            <div className="h-screen justify-center">
+                <br />
+                <br />
+                <center className="m-auto">
+                    <div className="tracking-widest">
+                        <span className="text-gray-500 text-6xl block">
+                            <span>4 0 4</span>
+                        </span>
+                        <span className="text-gray-500 text-xl">
+                            Sorry We could not find what you are looking for!
+                        </span>
+                    </div>
+                </center>
+            </div>
+        </>
+    );
+};
+
+export default Notfound;
